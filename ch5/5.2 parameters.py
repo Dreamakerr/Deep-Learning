8e49cce02b21897b3b1c47616b6a7bb08c37b2bd
Dreamakerr <104191299+Dreamakerr@users.noreply.github.com>
2023-03-30 17:02:34 +0800

    Update 5.2 parameters.py
    Signed-off-by: Dreamakerr <104191299+Dreamakerr@users.noreply.github.com>

--- ch5/5.2 parameters.py
+++ ch5/5.2 parameters.py
@@ -1,1 +1,113 @@
-da
+# 单隐藏层多层感知机
+import torch
+from torch import nn
+
+net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 1))
+X = torch.rand(size=(2, 4))
+net(X)
+
+
+# 参数访问
+print(net[2].state_dict())
+# 输出的结果告诉我们一些重要的事情： 首先，这个全连接层包含两个参数，分别是该层的权重和偏置。 两者都存储为单精度浮点数（float32）。 
+# 注意，参数名称允许唯一标识每个参数，即使在包含数百个层的网络中也是如此。
+
+
+# 目标参数
+print(type(net[2].bias))
+print(net[2].bias)
+print(net[2].bias.data)
+
+net[2].weight.grad == None
+
+
+# 一次性访问所有参数
+print(*[(name, param.shape) for name, param in net[0].named_parameters()])
+print(*[(name, param.shape) for name, param in net.named_parameters()])
+
+# 参数访问另一种形式
+net.state_dict()['2.bias'].data
+
+
+# 从嵌套块收集参数
+def block1():
+    return nn.Sequential(nn.Linear(4, 8), nn.ReLU(),
+                         nn.Linear(8, 4), nn.ReLU())
+
+def block2():
+    net = nn.Sequential()
+    for i in range(4):
+        # 在这里嵌套
+        net.add_module(f'block {i}', block1())
+    return net
+
+rgnet = nn.Sequential(block2(), nn.Linear(4, 1))
+rgnet(X)
+
+# 查看网络
+print(rgnet)
+# 访问第一个主要的块中、第二个子块的第一层的偏置项。
+rgnet[0][1][0].bias.data
+
+# 参数初始化
+# 内置初始化
+def init_normal(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, mean=0, std=0.01)
+        nn.init.zeros_(m.bias)
+net.apply(init_normal)
+net[0].weight.data[0], net[0].bias.data[0]
+
+# 参数初始化为常数
+def init_constant(m):
+    if type(m) == nn.Linear:
+        nn.init.constant_(m.weight, 1)
+        nn.init.zeros_(m.bias)
+net.apply(init_constant)
+net[0].weight.data[0], net[0].bias.data[0]
+
+
+# 对某些块应用不同的初始化方法
+# Xavier初始化方法，将第三个神经网络层初始化为常量值42
+def init_xavier(m):
+    if type(m) == nn.Linear:
+        nn.init.xavier_uniform_(m.weight)
+def init_42(m):
+    if type(m) == nn.Linear:
+        nn.init.constant_(m.weight, 42)
+
+net[0].apply(init_xavier)
+net[2].apply(init_42)
+print(net[0].weight.data[0])
+print(net[2].weight.data)
+
+
+# 自定义初始化
+def my_init(m):
+    if type(m) == nn.Linear:
+        print("Init", *[(name, param.shape)
+                        for name, param in m.named_parameters()][0])
+        nn.init.uniform_(m.weight, -10, 10)
+        m.weight.data *= m.weight.data.abs() >= 5
+
+net.apply(my_init)
+net[0].weight[:2]
+
+# 直接设置参数
+net[0].weight.data[:] += 1
+net[0].weight.data[0, 0] = 42
+net[0].weight.data[0]
+
+# 参数绑定
+# 我们需要给共享层一个名称，以便可以引用它的参数
+shared = nn.Linear(8, 8)
+net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(),
+                    shared, nn.ReLU(),
+                    shared, nn.ReLU(),
+                    nn.Linear(8, 1))
+net(X)
+# 检查参数是否相同
+print(net[2].weight.data[0] == net[4].weight.data[0])
+net[2].weight.data[0, 0] = 100
+# 确保它们实际上是同一个对象，而不只是有相同的值
+print(net[2].weight.data[0] == net[4].weight.data[0])
